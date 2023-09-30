670c60ac086657b06ca71c0559f08d4ed46fa8c2
ibrahmad <ibrahmad18@gmail.com>
2023-09-30 20:23:36 +0100

    adding a dynamic urls to navigate with args betwen home to room with id

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 
@@ -14,5 +14,11 @@ def home(request):
     return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = { 'room': room }
+    
+    return render(request, 'base/room.html', context)
